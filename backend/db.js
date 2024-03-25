6841d1128d9c61aedafe6cc758175ee406583df3
Varun Nair <varunsnair1997@gmail.com>
2024-03-26 00:23:30 +0530

    mongoose

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,8 @@
 const mongoose=require("mongoose")
+
+const conection=mongoose.connect("mongodb://127.0.0.1:27017/")
+
+
+
+
+module.exports={conection}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app=express()
+const {conection}=require("./db")
 
 
 app.get("/",(req,res)=>{
@@ -8,6 +9,15 @@ app.get("/",(req,res)=>{
 
 
 
-app.listen(2020,()=>{
-    console.log("listening to 2020....")
+app.listen(2020,async()=>{
+    try{
+    await conection
+    console.log("db connected successfully")
+    }
+    catch(err){
+        console.log("db failed to connect",err)
+    }
+    console.log("listening to 2020...")
+    
+    
 })
